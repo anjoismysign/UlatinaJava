64430e4194b79f3bab623ca34e29756989691e18
lbenav8095 <lubensaico@gmail.com>
2022-09-12 18:25:40 -0600

    1.0.5

--- src/main/java/us/fiestaboleana/java/libraries/PanelLib.java
+++ src/main/java/us/fiestaboleana/java/libraries/PanelLib.java
@@ -4,6 +4,12 @@ import javax.swing.*;
 
 public class PanelLib {
 
+    /**
+     * Pide un entero (int/integer) mediante un JOptionPane
+     * Si no se introduce un entero, se hara recursión hasta conseguir el entero
+     * @param message Mensaje que se muestra mientras se pide el entero
+     * @return el entero
+     */
     public static int requestInteger(String message) {
         String input = requestString(message);
         int x = 0;
@@ -18,6 +24,12 @@ public class PanelLib {
         }
     }
 
+    /**
+     * Pide un numero decimal (double) mediante un JOptionPane
+     * Si no se introduce un numero decimal, se hara recursión hasta conseguir el numero decimal
+     * @param message Mensaje que se muestra mientras se pide el numero decimal
+     * @return el numero decimal
+     */
     public static double requestDouble(String message) {
         String input = requestString(message);
         double x = 0;
@@ -31,20 +43,48 @@ public class PanelLib {
         }
     }
 
+    /**
+     * Pide una cadena de texto (String) mediante un JOptionPane
+     * Si la cadena de texto está vacía o es null, se hara recursión hasta conseguir una cadena de texto
+     * @param message Mensaje que se muestra mientras se pide la cadena de texto
+     * @return la cadena de texto
+     */
     public static String requestString(String message) {
         String input = requestString("", message);
         return input;
     }
 
+    /**
+     * Pide una cadena de texto (String) mediante un JOptionPane
+     * La diferencia es que este método permite poner un título al JOptionPane
+     * Si la cadena de texto está vacía o es null, se hara recursión hasta conseguir una cadena de texto
+     * @param title Título del JOptionPane
+     * @param message Mensaje que se muestra mientras se pide la cadena de texto
+     * @return la cadena de texto
+     */
     public static String requestString(String title, String message) {
         String input = JOptionPane.showInputDialog(null, message, title, -1);
+        if (input == null || input.length() == 0) {
+            showMessage("ERROR", "Lo introducido está vacío");
+            return requestString(title, message);
+        }
         return input;
     }
 
+    /**
+     * Muestra un mensaje mediante un JOptionPane
+     * @param message Mensaje que se muestra
+     */
     public static void showMessage(String message) {
         showMessage("", message);
     }
 
+    /**
+     * Muestra un mensaje mediante un JOptionPane
+     * La diferencia de este método es que permite poner un título al JOptionPane
+     * @param title Título del JOptionPane
+     * @param message Mensaje que se muestra
+     */
     public static void showMessage(String title, String message) {
         JOptionPane.showMessageDialog(null, message, title, -1);
     }
