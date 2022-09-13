c2d61108e1a3a40e419d68a814565856977bc1f8
lbenav8095 <lubensaico@gmail.com>
2022-09-13 00:13:09 -0600

    1.0.8-JAVADOCS

--- src/main/java/us/fiestaboleana/java/UlatinaJava.java
+++ src/main/java/us/fiestaboleana/java/UlatinaJava.java
@@ -1,8 +1,12 @@
 package us.fiestaboleana.java;
 
+import us.fiestaboleana.java.libraries.PanelLib;
+
 public class UlatinaJava {
 
     public static void main(String[] args) {
-        System.out.println("Bienvenido estudiante.");
+        PanelLib.esEs();
+        boolean test = PanelLib.requestBoolean("¿Me gusta programar?");
+        PanelLib.showMessage("INFO",test ? "Sí, me gusta programar." : "No, creo que no me gusta programar.");
     }
 }
--- src/main/java/us/fiestaboleana/java/libraries/PanelLib.java
+++ src/main/java/us/fiestaboleana/java/libraries/PanelLib.java
@@ -3,15 +3,120 @@ package us.fiestaboleana.java.libraries;
 import javax.swing.*;
 
 public class PanelLib {
+    public static void esEs(){
+        UIManager.put("OptionPane.cancelButtonText", "Anular");
+        UIManager.put("OptionPane.noButtonText", "No");
+        UIManager.put("OptionPane.okButtonText", "Acepto");
+        UIManager.put("OptionPane.yesButtonText", "Si");
+    }
+
+    /**
+     * Pide un boolean de la forma mas elegante
+     * En caso de cerrarse la ventana, se hará recursión
+     * En caso del mensaje ser escrito en español,
+     * se recomienda PanelLib#esEs al inicio del
+     * método main
+     * La diferencia de este método es que permite poner un título al JOptionPane
+     * @param title Título del JOptionPane
+     * @param message Mensaje que se muestra mientras se pide
+     *                el boolean
+     * @return el boolean seleccionado
+     */
+    public static boolean requestBoolean(String title, String message){
+        JOptionPane.setRootFrame(null);
+        int x = JOptionPane.showConfirmDialog(null, message, title,
+                0, -1);
+        if (x == JOptionPane.CLOSED_OPTION)
+            return requestBoolean(title, message);
+        return x == 0 ? true : false;
+    }
+
+    /**
+     * Pide un boolean de la forma mas elegante
+     * En caso de cerrarse la ventana, se hará recursión
+     * En caso del mensaje ser escrito en español,
+     * se recomienda PanelLib#esEs al inicio del
+     * método main
+     * @param message Mensaje que se muestra mientras se pide
+     *                el boolean
+     * @return el boolean seleccionado
+     */
+    public static boolean requestBoolean(String message){
+        return requestBoolean("", message);
+    }
+
+    /**
+     * Pide un boolean mediante un JOptionPane
+     * De no ingresarse "si" o "no", se hace recursión
+     * La diferencia de este método es que permite poner un título al JOptionPane
+     * @param titulo Título del JOptionPane
+     * @param mensaje Mensaje mostrado antes de "Ingrese 'si' o 'no'"
+     * @return true en caso de "si", false en caso de "no"
+     */
+    public static boolean confirmar(String titulo, String mensaje){
+        String input = PanelLib.requestString(mensaje+" \nIngrese 'si' o 'no'");
+        String minusculas = input.toLowerCase();
+        if (!minusculas.equals("si") && !minusculas.equals("no")) {
+            PanelLib.showMessage(titulo,"'"+input+"' no es ni 'si' ni 'no'");
+            return confirmar(titulo, mensaje);
+        }
+        if (minusculas.equals("si"))
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Pide un boolean mediante un JOptionPane
+     * De no ingresarse "si" o "no", se hace recursión
+     * @param mensaje Mensaje mostrado antes de "Ingrese 'si' o 'no'"
+     * @return true en caso de "si", false en caso de "no"
+     */
+    public static boolean confirmar(String mensaje){
+        return confirmar("", mensaje);
+    }
+
+    /**
+     * Pide un boolean mediante un JOptionPane
+     * De no ingresarse "yes" o "no", se hace recursión
+     * La diferencia de este método es que permite poner un título al JOptionPane
+     * @param title Título del JOptionPane
+     * @param message Mensaje mostrado antes de "Type 'yes' or 'no'"
+     * @return true en caso de "yes", false en caso de "no"
+     */
+    public static boolean confirm(String title, String message){
+        String input = PanelLib.requestString(message+" \nType 'yes' or 'no'");
+        String lowercased = input.toLowerCase();
+        if (!lowercased.equals("yes") && !lowercased.equals("no")) {
+            PanelLib.showMessage(title,"'"+input+"' is neither 'yes' or 'no'");
+            return confirm(title, message);
+        }
+        if (lowercased.equals("yes"))
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Pide un boolean mediante un JOptionPane
+     * De no ingresarse "yes" o "no", se hace recursión
+     * @param message Mensaje mostrado antes de "Type 'yes' or 'no'"
+     * @return true en caso de "yes", false en caso de "no"
+     */
+    public static boolean confirm(String message){
+        return confirm("", message);
+    }
 
     /**
      * Pide un entero (int/integer) mediante un JOptionPane
      * Si no se introduce un entero, se hara recursión hasta conseguir el entero
+     * La diferencia de este método es que permite poner un título al JOptionPane
+     * @param title Título del JOptionPane
      * @param message Mensaje que se muestra mientras se pide el entero
      * @return el entero
      */
-    public static int requestInteger(String message) {
-        String input = requestString(message);
+    public static int requestInteger(String title, String message) {
+        String input = requestString(title, message);
         int x = 0;
         try {
             x = Integer.parseInt(input);
@@ -20,7 +125,38 @@ public class PanelLib {
             showMessage("ERROR", "'" + input + "' no es un entero\n" +
                     "Integer (entero): '-2147483648' hasta '2147483647'\n" +
                     "Ejemplo de entero: 17");
-            return requestInteger(message);
+            return requestInteger(title, message);
+        }
+    }
+
+    /**
+     * Pide un entero (int/integer) mediante un JOptionPane
+     * Si no se introduce un entero, se hara recursión hasta conseguir el entero
+     * @param message Mensaje que se muestra mientras se pide el entero
+     * @return el entero
+     */
+    public static int requestInteger(String message) {
+        return requestInteger("", message);
+    }
+
+    /**
+     * Pide un numero decimal (double) mediante un JOptionPane
+     * Si no se introduce un numero decimal, se hara recursión hasta conseguir el numero decimal
+     * La diferencia de este método es que permite poner un título al JOptionPane
+     * @param title Título del JOptionPane
+     * @param message Mensaje que se muestra mientras se pide el numero decimal
+     * @return el numero decimal
+     */
+    public static double requestDouble(String title, String message) {
+        String input = requestString(title, message);
+        double x = 0;
+        try {
+            x = Double.parseDouble(input);
+            return x;
+        } catch (NumberFormatException e) {
+            showMessage("ERROR", "'" + input + "' no es un numero decimal\n" +
+                    "Ejemplo de double (numero decimal): '3.14'");
+            return requestDouble(title, message);
         }
     }
 
@@ -31,16 +167,7 @@ public class PanelLib {
      * @return el numero decimal
      */
     public static double requestDouble(String message) {
-        String input = requestString(message);
-        double x = 0;
-        try {
-            x = Double.parseDouble(input);
-            return x;
-        } catch (NumberFormatException e) {
-            showMessage("ERROR", "'" + input + "' no es un numero decimal\n" +
-                    "Ejemplo de double (numero decimal): '3.14'");
-            return requestDouble(message);
-        }
+        return requestDouble("", message);
     }
 
     /**
