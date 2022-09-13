c0e3f2ba8b3a124c3d7c183c7c56c32c610197c2
lbenav8095 <lubensaico@gmail.com>
2022-09-13 13:24:23 -0600

    1.0.14-JAVADOCS

--- src/main/java/us/fiestaboleana/java/libraries/SwingLib.java
+++ src/main/java/us/fiestaboleana/java/libraries/SwingLib.java
@@ -17,11 +17,12 @@ public class SwingLib {
      * @param optionType Tipo de botones que se mostrarán
      * @param messageType 0 en caso de error, 1 en caso de información, 2 en caso de advertencia,
      *                    3 en caso de pregunta, -1 para no mostrar ningún icono
-     * @return 0 en caso de "YES", 1 en caso de "NO", 2 en caso de "CANCEL", -1 en caso de
+     * @param expecting 0 en caso de "YES", 1 en caso de "NO", 2 en caso de "CANCEL", -1 en caso de
      * haber cerrado la ventana
      */
-    public static int anjoPane(Collection<AnjoComponent> components,
-                                String title, int optionType, int messageType) {
+    public static JPanel anjoPane(Collection<AnjoComponent> components,
+                                String title, int optionType, int messageType,
+                                int expecting){
         JPanel panel = new JPanel(new GridLayout(components.size(), 2));
         components.forEach(component -> {
             panel.add(component.getLabel());
@@ -29,7 +30,9 @@ public class SwingLib {
         });
         int result = JOptionPane.showConfirmDialog(null, panel,
                 title, optionType, messageType);
-        return result;
+        if (result == expecting)
+            return panel;
+        return null;
     }
 
     /**
@@ -40,11 +43,33 @@ public class SwingLib {
      * @param optionType Tipo de botones que se mostrarán
      * @param messageType 0 en caso de error, 1 en caso de información, 2 en caso de advertencia,
      *                    3 en caso de pregunta, -1 para no mostrar ningún icono
-     * @return 0 en caso de "YES", 1 en caso de "NO", 2 en caso de "CANCEL", -1 en caso de
+     * @param expecting 0 en caso de "YES", 1 en caso de "NO", 2 en caso de "CANCEL", -1 en caso de
      * haber cerrado la ventana
      */
-    public static int anjoPane(AnjoComponent[] components,
-                               String title, int optionType, int messageType) {
-        return anjoPane(Arrays.asList(components), title, optionType, messageType);
+    public static JPanel anjoPane(AnjoComponent[] components,
+                                  String title, int optionType, int messageType,
+                                  int expecting){
+        return anjoPane(Arrays.asList(components), title, optionType, messageType, expecting);
+    }
+
+    /**
+     * Consigue el componente de una fila de un AnjoPane.
+     * Primero hay que asegurarse que el JPanel le pertenezca
+     * a un panel creado con SwingLib#anjoPane.
+     * @param panel El panel de AnjoPane
+     * @param index El índice de la fila
+     * @return El componente en caso de que si exista.
+     * Null si no se encuentra.
+     */
+    public static Component getAnjoJComponentFromJPanel(JPanel panel, int index){
+        Component component;
+        try {
+            component = panel.getComponent(index*2);
+            return component;
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("El índice no existe \n" +
+                    "Total de índices (en caso de ser un JPanel de AnjoPane): " + panel.getComponentCount()/2);
+            return null;
+        }
     }
 }
--- target/site/apidocs/package-search-index.js
+++ target/site/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"us.fiestaboleana.java"},{"l":"us.fiestaboleana.java.libraries"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"us.fiestaboleana.java"},{"l":"us.fiestaboleana.java.libraries"},{"l":"us.fiestaboleana.java.objects"}];updateSearchResults();
